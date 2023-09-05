ef5dc1880c5025adbfcd5f23874dfa4a228cf4d6
AGS Lakpahana <agslakpahana@gmail.com>
2023-09-05 20:38:47 +0530

    dao and implementation

---
+++ spirngbootcrud/src/main/java/com/ruhuna/spirngbootcrud/dao/EmployeeDAO/iEmployeeDAO.java
@@ -0,0 +1,6 @@
+package com.ruhuna.spirngbootcrud.dao.EmployeeDAO;
+//hiding implementation details by interfaces
+//interfaces is a way to achieve abstraction
+public interface iEmployeeDAO {
+    void findAll();
+}
---
+++ spirngbootcrud/src/main/java/com/ruhuna/spirngbootcrud/dao/EmployeeDAOImpl.java
@@ -0,0 +1,11 @@
+package com.ruhuna.spirngbootcrud.dao;
+
+import com.ruhuna.spirngbootcrud.dao.EmployeeDAO.iEmployeeDAO;
+
+public class EmployeeDAOImpl implements iEmployeeDAO {
+
+    @Override
+    public void findAll() {
+
+    }
+}
