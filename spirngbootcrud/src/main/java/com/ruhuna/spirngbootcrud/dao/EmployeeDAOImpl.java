cbe8d7f158dc93ac044a080109d778fa5c3f0bc7
AGS Lakpahana <agslakpahana@gmail.com>
2023-09-05 20:45:19 +0530

    dep injection

--- spirngbootcrud/src/main/java/com/ruhuna/spirngbootcrud/dao/EmployeeDAOImpl.java
+++ spirngbootcrud/src/main/java/com/ruhuna/spirngbootcrud/dao/EmployeeDAOImpl.java
@@ -1,8 +1,18 @@
 package com.ruhuna.spirngbootcrud.dao;
 
 import com.ruhuna.spirngbootcrud.dao.EmployeeDAO.iEmployeeDAO;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class EmployeeDAOImpl implements iEmployeeDAO {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
 
     @Override
     public void findAll() {
