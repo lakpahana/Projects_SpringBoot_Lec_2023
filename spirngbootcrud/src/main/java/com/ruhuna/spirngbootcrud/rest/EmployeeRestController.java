93ceea3d3d26e3a7340a6b82df84f068d380934b
AGS Lakpahana <agslakpahana@gmail.com>
2023-09-09 17:54:36 +0530

    Update EmployeeRestController.java

--- spirngbootcrud/src/main/java/com/ruhuna/spirngbootcrud/rest/EmployeeRestController.java
+++ spirngbootcrud/src/main/java/com/ruhuna/spirngbootcrud/rest/EmployeeRestController.java
@@ -2,35 +2,68 @@ package com.ruhuna.spirngbootcrud.rest;
 
 import com.ruhuna.spirngbootcrud.dao.EmployeeDAO.IEmployeeDAO;
 import com.ruhuna.spirngbootcrud.entities.Employee;
+import com.ruhuna.spirngbootcrud.service.iEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequestMapping("api")
 @RestController
 public class EmployeeRestController {
-    private IEmployeeDAO employeeDAO;
+//    private IEmployeeDAO employeeDAO;
 
+    private iEmployeeService employeeService;
     @Autowired
-    //constructor injection
-    public EmployeeRestController(IEmployeeDAO employeeDAO) {
-
-        this.employeeDAO = employeeDAO;
+    public EmployeeRestController(iEmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
+    //constructor injection
+//    public EmployeeRestController(IEmployeeDAO employeeDAO) {
+//
+//        this.employeeDAO = employeeDAO;
+//    }
     //localhost:8080/api/employees
     @GetMapping("/employees")
     List<Employee> findAll() {
 //        System.out.println("sadas");
-        return null;
+        return employeeService.findAll();
     }
 
     @GetMapping("/employees/{requestedId}")
     Employee findEmployeeById(@PathVariable int requestedId){
-        return null;
+        //if this is not found return null
+        Employee employee = employeeService.findById(requestedId);
 
+        if(employee == null){
+            throw new RuntimeException("Employee id not found - "+requestedId);
+        }
+
+        return employee;
+
+    }
+
+
+    @PostMapping("/employees")
+    Employee addEmployee(@RequestBody Employee employee){
+        employeeService.save(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    Employee updateEmployee(@RequestBody Employee employee){
+        //if it's there it will update it,if not it will create a new one
+        employeeService.save(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{requestedId}")
+    String deleteEmployee(@PathVariable int requestedId){
+        Employee employee = employeeService.findById(requestedId);
+        if(employee == null){
+            throw new RuntimeException("Employee id not found - "+requestedId);
+        }
+        employeeService.deleteById(requestedId);
+        return "Deleted employee id - "+requestedId;
     }
 }
